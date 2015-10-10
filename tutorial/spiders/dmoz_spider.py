001e2993ab5588ec64a20761bff3675a48c5fa07
Mushahid Alam <mushahidalam@wifi-596.cs.wisc.edu>
2015-10-10 17:47:58 -0500

    [Working Spider]

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -3,33 +3,49 @@ from scrapy.utils.response import open_in_browser
 from tutorial.items import DmozItem
 import logging
 import os
+import scrapy
+from scrapy.utils.response import open_in_browser
+from tutorial.items import DmozItem
+import logging
+import os
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.loader import XPathItemLoader
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.spider import Spider
+from scrapy import Request
 
 class DmozSpider(scrapy.Spider):
     name = "amazon"
     allowed_domains = ["amazon.com"]
     start_urls = [
-        "http://www.amazon.com/s/ref=lp_2650363011_nr_p_n_feature_four_bro_1?fst=as%3Aoff&rh=n%3A2625373011%2Cp_n_theme_browse-bin%3A2650363011%2Cp_n_feature_four_browse-bin%3A2650442011&bbn=2650363011&ie=UTF8&qid=1444429468&rnid=2650439011",
+        "http://www.amazon.com/s/ref=sr_pg_1?fst=as%3Aoff&rh=n%3A2625373011%2Cp_n_theme_browse-bin%3A2650363011%2Cp_n_feature_four_browse-bin%3A2650442011&bbn=2650363011&ie=UTF8&qid=1444512697",
     ]
 
+    '''var1  = "http://www.amazon.com/s?bbn=2650363011&ie=UTF8&page="
+    var2  = "&rh=n%3A2625373011%2Cp_n_theme_browse-bin%3A2650363011%2Cp_n_feature_four_browse-bin%3A2650442011"
+    for i in range(2,3):
+        var3 = var1 + `i` + var2
+        print(var3)
+        start_urls.append(var3)'''
+
+
     def parse(self, response):
-        #open_in_browser(response)
         for href in response.css("div.a-row.a-spacing-small > a::attr('href')"):
             if href:
-                #url = response.urljoin(href.extract())
                 url =  href.extract()
-                logging.warning(' item received for %s', url)
                 yield scrapy.Request(url, callback=self.parse_dir_contents)
             else:
                 logging.warning('No item received for ', href)
-        
 
     def parse_dir_contents(self, response):
-        logging.warning('reponse.url before splitting %s', response.url)
         str1 = response.url.split("/")[3]
-        #str1 = str1.split("?")[0]
         filename = 'output/'+str1+ '.html'
-        #os.path.exists(str1+'.html')
-        logging.warning('No item received for %s', filename)
+        '''hxs = HtmlXPathSelector(response)
+        director_list = hxs.xpath('//*[@id="dv-center-features"]/div[1]/div/table/tbody')'''
+        #director = director_list.extract()
+        print(director_list[0])
         with open(filename, 'wb') as f:
             f.write(response.body)
 
